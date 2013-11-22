3f77044a642cf610d8c870d4177f4bd0894490bb
angelinay0403 <angelinay0403@gmail.com>
2013-11-22 14:51:04 -0500

    errors

--- Function_Personal/js/script.js
+++ Function_Personal/js/script.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+// Angelina Castro, 11/21/2013, Functions.
+
+
+// Personal_Functions.
+// hair done cost.
+
+var hairColor = 60;
+var washSet = 15;
+var total = calculateTotal(hairColor,washSet);
+function calculateTotal()
+{
+console.log ("Done Cost is" + total);
+	
+}
