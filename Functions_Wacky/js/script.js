7a6b37081a5134f57e79a48ab97a98ccd7887e2d
angelinay0403 <angelinay0403@gmail.com>
2013-11-24 21:41:46 -0500

    error fixed / finished.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,11 +6,12 @@
 //Diapers change per day.
 // how many diapers i use per week.
 
-
+	calcusedDiapers(3,2,7);
 	function calcusedDiapers(w,d,t,u){
 var wetDiapers = 3;
 var dirtyDiapers = 2;
 var timesaWeek  = 7;// seven days a week.
 var usedDiapers = (wetDiapers + dirtyDiapers) * (timesaWeek);
-}
+
 	console.log(usedDiapers)
+}
