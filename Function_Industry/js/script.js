45421cae75ed8531c7bf18aeeaaf238d6e401695
angelinay0403 <angelinay0403@gmail.com>
2013-11-24 20:47:42 -0500

    fixing errors

--- Function_Industry/js/script.js
+++ Function_Industry/js/script.js
@@ -5,13 +5,15 @@
 //Function_Industry.
 
 //Web Developer.
-
+var webSite = calctotalWork(10, 50,5,10);
+	function calctotalWork(h,p,w,m,t){
 var hours = 10; // hours working on a web page.
 var payPerhour = 50; // how much per hour.
 var webPages = 5; // pages in a website.
 var moneyPerpages =10; // how much extra per pages.
-var totalWork = (hours * payPerhour) + (webPages * moneyPerpages)
-
-console.log(totalWork)
+var totalWork = (hours * payPerhour) + (webPages * moneyPerpages);
+	return totalWork;
+}
+console.log(webSite)
 
  
