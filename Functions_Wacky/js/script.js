5afc437738eaf126e256c1bf58abbd209fac565a
angelinay0403 <angelinay0403@gmail.com>
2013-11-24 21:22:33 -0500

    trying to find the errors.

---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+// Angelina Castro, 11/21/2013, Functions.
+
+//Functions_Wacky.
+
+//Diapers change per day.
+// how many diapers i use per week.
+	calctotalDiapers(3,2,7);
+	function calctotal(w,d,d,t){
+var wetDiapers = 3;
+var dirtyDiapers = 2;
+var daysAweek  = 7;
+var totalDiapers = (wetDiapers + dirtyDiapers) * daysAweek;
+}
+	console.log(totalDiapers)
