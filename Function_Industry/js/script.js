4752fa2d12845b30c4ba051d2faf2bd3178ad406
angelinay0403 <angelinay0403@gmail.com>
2013-11-24 20:33:42 -0500

    Function_Industry unfinished.

--- Function_Industry/js/script.js
+++ Function_Industry/js/script.js
@@ -1,1 +1,17 @@
 // JavaScript Document
+// Angelina Castro, 11/21/2013, Functions.
+
+
+//Function_Industry.
+
+//Web Developer.
+
+var hours = 10; // hours working on a web page.
+var payPerhour = 50; // how much per hour.
+var webPages = 5; // pages in a website.
+var moneyPerpages =10; // how much extra per pages.
+var totalWork = (hours * payPerhour) + (webPages * moneyPerpages)
+
+console.log(totalWork)
+
+ 
